17f53d3aff5e5d38c0374554bc6d9f3961cd3397
Cole Fortner <colefortner@gmail.com>
2022-09-14 22:38:38 -0400

    Implement FormField component for form inputs, introduce form state management

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,20 @@
+import React, { useState } from "react";
 import { Layout } from "../components/layout";
+import { FormField } from "~/components/form-field";
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Updates the form data when an input changes
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({ ...form, [field]: event.target.value }));
+  };
   return (
     <Layout>
       <div>
@@ -8,12 +22,19 @@ export default function Login() {
         <p>Log In To Give Some Praise!</p>
 
         <form method="post">
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
-
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" />
-
+          <FormField
+            htmlFor="email"
+            label="Email"
+            value={formData.email}
+            onChange={(e) => handleInputChange(e, "email")}
+          />
+          <FormField
+            htmlFor="password"
+            type="password"
+            label="Password"
+            value={formData.password}
+            onChange={(e) => handleInputChange(e, "password")}
+          />
           <div className="w-full text-center">
             <input type="submit" value="Sign In" />
           </div>
